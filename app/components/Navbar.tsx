1469303f06d5adf26f7963a8b115ad2825713b36
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-04-07 14:42:29 -0700

    roket

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { normalizeConfig } from "next/dist/server/config"
+import Link from "next/link"
+import Search from "./search"
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-300 p-4 flex justify-between flex-col
+    md:flex-row sticky top-0 drop-shadow-xl">
+        <h1 className="text-3xl font-bold text-blue-400 
+        grid place-content-center md:mb-0">
+        <Link href='/'>WikiRoket!</Link>
+        </h1>
+        <Search/>
+        </nav>
+  )
+}
